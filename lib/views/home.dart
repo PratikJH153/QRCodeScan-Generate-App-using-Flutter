8a060646cac8e137b68aa90ae5c2756756ce2e0d
PratikJH <pratik.jh2017@gmail.com>
2021-12-30 23:03:04 +0530

    Added Scanning QRCode functionality

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:qrcodeapp/models/option.dart';
-import 'package:qrcodeapp/views/generate_code_page.dart';
 import 'package:qrcodeapp/views/scan_code_page.dart';
 import 'package:qrcodeapp/widgets/option_widget.dart';
 import 'package:qrcodeapp/widgets/textfield.dart';
+import 'dart:typed_data';
+import 'dart:ui';
+import 'dart:io';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,6 +22,65 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool _isGenerateSelected = true;
+  final TextEditingController _textController = TextEditingController();
+  final TextEditingController _urlController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
+  final TextEditingController _latitudeController = TextEditingController();
+  final TextEditingController _longitudeController = TextEditingController();
+
+  Option _selectedOption = Option.options[0];
+  GlobalKey globalKey = GlobalKey();
+
+  @override
+  void dispose() {
+    disposingTextController();
+    super.dispose();
+  }
+
+  void disposingTextController() {
+    _textController.dispose();
+    _urlController.dispose();
+    _phoneController.dispose();
+    _emailController.dispose();
+    _messageController.dispose();
+    _subjectController.dispose();
+    _latitudeController.dispose();
+    _longitudeController.dispose();
+  }
+
+  Future<void> _captureAndSharePng() async {
+    try {
+      _getShareFilePermission();
+      RenderRepaintBoundary? boundary =
+          globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      var image = await boundary.toImage();
+      ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
+      Uint8List? pngBytes = byteData!.buffer.asUint8List();
+
+      Directory? tempDir = await getTemporaryDirectory();
+      File? file = await File('${tempDir.path}/image.png').create();
+      await file.writeAsBytes(pngBytes);
+
+      final channel = MethodChannel('channel:me.alfian.share/share');
+      channel.invokeMethod('shareFile', 'image.png');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<PermissionStatus> _getShareFilePermission() async {
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      final result = await Permission.storage.request();
+      return result;
+    } else {
+      return status;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,47 +89,47 @@ class _HomePageState extends State<HomePage> {
         child: FloatingActionButton.extended(
           backgroundColor: Theme.of(context).primaryColor,
           onPressed: () {
-            // showDialog(
-            //     context: context,
-            //     barrierColor: Colors.black45,
-            //     builder: (ctx) => Center(
-            //           child: Dialog(
-            //             backgroundColor: Colors.transparent,
-            //             child: SizedBox(
-            //               width: 270,
-            //               height: 320,
-            //               child: Column(
-            //                 children: [
-            //                   Row(
-            //                     mainAxisAlignment:
-            //                         MainAxisAlignment.spaceBetween,
-            //                     children: [
-            //                       IconButton(
-            //                         onPressed: () {},
-            //                         icon:
-            //                             const Icon(CupertinoIcons.share_solid),
-            //                       ),
-            //                       IconButton(
-            //                         onPressed: () {
-            //                           Navigator.of(context).pop();
-            //                         },
-            //                         icon: const Icon(
-            //                           CupertinoIcons.xmark_square_fill,
-            //                         ),
-            //                       ),
-            //                     ],
-            //                   ),
-            //                   QrImage(
-            //                     data: _textController.text.trim(),
-            //                     version: QrVersions.auto,
-            //                     foregroundColor: Colors.white,
-            //                     size: 270,
-            //                   ),
-            //                 ],
-            //               ),
-            //             ),
-            //           ),
-            //         ));
+            showDialog(
+                context: context,
+                barrierColor: Colors.black45,
+                builder: (ctx) => Center(
+                      child: Dialog(
+                        backgroundColor: Colors.transparent,
+                        child: SizedBox(
+                          width: 270,
+                          height: 320,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                    onPressed: _captureAndSharePng,
+                                    icon:
+                                        const Icon(CupertinoIcons.share_solid),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    icon: const Icon(
+                                      CupertinoIcons.xmark_square_fill,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              QrImage(
+                                data: _textController.text.trim(),
+                                version: QrVersions.auto,
+                                foregroundColor: Colors.white,
+                                size: 270,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ));
           },
           icon: const Icon(
             CupertinoIcons.greaterthan_square_fill,
@@ -151,7 +216,124 @@ class _HomePageState extends State<HomePage> {
                 height: 30,
               ),
               _isGenerateSelected
-                  ? const GenerateQRCodePage()
+                  ? Column(
+                      children: [
+                        Center(
+                          child: Wrap(
+                            direction: Axis.horizontal,
+                            alignment: WrapAlignment.center,
+                            runSpacing: 20,
+                            spacing: 20,
+                            children: Option.options
+                                .map(
+                                  (option) => GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        _selectedOption = option;
+                                      });
+                                    },
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 16,
+                                        vertical: 6,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: _selectedOption == option
+                                            ? Theme.of(context).primaryColor
+                                            : Colors.transparent,
+                                        border: Border.all(
+                                          width: 3,
+                                          color: _selectedOption == option
+                                              ? const Color(0xFF26a66c)
+                                              : const Color(0xFF2e2e2e),
+                                        ),
+                                      ),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Icon(option.icon,
+                                              size: 20,
+                                              color: _selectedOption == option
+                                                  ? Colors.white
+                                                  : Colors.grey),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text(
+                                            option.title,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: _selectedOption == option
+                                                  ? Colors.white
+                                                  : Colors.grey,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        if (_selectedOption.title == "Text")
+                          TextFieldWidget(
+                            title: "Text",
+                            icon: CupertinoIcons.textformat_alt,
+                            textEditingController: _textController,
+                          ),
+                        if (_selectedOption.title == "URL")
+                          TextFieldWidget(
+                            title: "URL",
+                            icon: CupertinoIcons.link,
+                            textEditingController: _urlController,
+                          ),
+                        if (_selectedOption.title == "Phone")
+                          TextFieldWidget(
+                            title: "Phone Number",
+                            icon: CupertinoIcons.phone,
+                            textEditingController: _phoneController,
+                          ),
+                        if (_selectedOption.title == "Email")
+                          Column(
+                            children: [
+                              TextFieldWidget(
+                                title: "Email Address",
+                                icon: CupertinoIcons.mail,
+                                textEditingController: _emailController,
+                              ),
+                              TextFieldWidget(
+                                title: "Subject",
+                                icon: CupertinoIcons.textformat,
+                                textEditingController: _subjectController,
+                              ),
+                              TextFieldWidget(
+                                title: "Message",
+                                icon: CupertinoIcons.t_bubble,
+                                textEditingController: _messageController,
+                              ),
+                            ],
+                          ),
+                        if (_selectedOption.title == "Location")
+                          Column(
+                            children: [
+                              TextFieldWidget(
+                                title: "Latitude",
+                                icon: CupertinoIcons.location,
+                                textEditingController: _latitudeController,
+                              ),
+                              TextFieldWidget(
+                                title: "Longitude",
+                                icon: CupertinoIcons.location,
+                                textEditingController: _longitudeController,
+                              ),
+                            ],
+                          ),
+                      ],
+                    )
                   : const ScanQRCodePage(),
             ],
           ),
--- lib/views/scan_code_page.dart
+++ lib/views/scan_code_page.dart
@@ -13,6 +13,8 @@ class _ScanQRCodePageState extends State<ScanQRCodePage> {
   final qrKey = GlobalKey(debugLabel: "QR");
   QRViewController? _qrViewController;
 
+  Barcode? barcode;
+
   @override
   void dispose() {
     _qrViewController!.dispose();
@@ -37,16 +39,52 @@ class _ScanQRCodePageState extends State<ScanQRCodePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: double.infinity,
-      child: QRView(
-          key: qrKey,
-          onQRViewCreated: (QRViewController controller) {
-            setState(() {
-              _qrViewController = controller;
-            });
-          }),
+    return Column(
+      children: [
+        Container(
+          height: 300,
+          width: double.infinity,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: QRView(
+              key: qrKey,
+              overlay: QrScannerOverlayShape(
+                borderRadius: 10,
+                borderColor: Colors.white,
+                borderWidth: 5,
+              ),
+              onQRViewCreated: (QRViewController controller) {
+                setState(() {
+                  _qrViewController = controller;
+                });
+                controller.scannedDataStream.listen((barcode) {
+                  setState(() {
+                    this.barcode = barcode;
+                  });
+                });
+              },
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 20),
+          padding: const EdgeInsets.symmetric(
+            vertical: 15,
+            horizontal: 25,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.black26,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Text(
+            barcode == null ? "Scan a code!" : barcode!.code.toString(),
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
